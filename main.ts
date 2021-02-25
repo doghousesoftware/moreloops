6353b19e3c46c797b65c37f499cf7ff2686406b0
bryan weinstein <bryanweinstein@outlook.com>
2021-02-25 11:06:34 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
+input.onPinPressed(TouchPin.P0, function () {
+    foo = 4
+    for (let index = 0; index <= 4; index++) {
+        basic.showNumber(foo)
+        foo += -1
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    if (intVariable != 3) {
-        basic.showString("True")
-    } else {
-        basic.showString("False")
+    for (let index = 0; index < 4; index++) {
+        basic.showNumber(k)
+        k += 1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    while (k <= 4) {
+        basic.showNumber(k)
+        k += 1
     }
 })
 input.onButtonPressed(Button.B, function () {
-    intVariable = 3
+    for (let index = 0; index <= 4; index++) {
+        basic.showNumber(index)
+    }
 })
-let intVariable = 0
-intVariable = 6
+let foo = 0
+let k = 0
+k = 0
